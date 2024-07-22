e689bce9f8deb300cf5ab25e2e34c1acd53ef0e2
Roberto Pineda <rpineda@x-codec.net>
2024-07-22 18:09:21 -0500

    Implementacion de APIGatewat Ocelot

--- Servicios.api.Seguridad/Startup.cs
+++ Servicios.api.Seguridad/Startup.cs
@@ -21,6 +21,7 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Servicios.api.Seguridad.Core.Application;
 using Servicios.api.Seguridad.Core.Entities;
 using Servicios.api.Seguridad.Core.JwtLogic;
@@ -43,7 +44,10 @@ namespace Servicios.api.Seguridad
         {
             
             services.AddControllers().AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<Register>());
-
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Servicios.api.Seguridad", Version = "v1" });
+            });
             services.AddDbContext<SeguridadContexto>(opt =>
             {
                 opt.UseSqlServer(Configuration.GetConnectionString("ConexionDB"));
@@ -97,6 +101,8 @@ namespace Servicios.api.Seguridad
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Servicios.api.Libreria v1"));
             }
 
             app.UseRouting();
